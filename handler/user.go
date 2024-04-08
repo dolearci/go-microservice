7a5f2bdd5c4a97f0c6055eb0aa887878b60dbe70
Daniel Olearčin <dolearci@redhat.com>
2024-04-08 10:52:16 +0200

    ✨ Add handler package

---
+++ handler/user.go
@@ -0,0 +1,24 @@
+package handlers
+
+import (
+	"github.com/dolearci/go-microservice/repository"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+type UserHandler struct {
+	repo *repository.UserRepository
+}
+
+func NewUserHandler(repo *repository.UserRepository) *UserHandler {
+	return &UserHandler{repo: repo}
+}
+
+func (h *UserHandler) GetAllUsers(c *gin.Context) {
+	users, err := h.repo.FindAll()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, users)
+}
