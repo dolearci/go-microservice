01ed50b0b14b36b21aef21dc661aaf2455af4e8c
Daniel Olearčin <dolearci@redhat.com>
2024-04-08 10:53:33 +0200

    ✨ Connect to database, create router and open 8080 for listening

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"github.com/dolearci/go-microservice/config"
+	handlers "github.com/dolearci/go-microservice/handler"
+	"github.com/dolearci/go-microservice/repository"
+	"github.com/gin-gonic/gin"
+)
 
+func main() {
+	database := config.ConnectToDatabase()
+
+	router := gin.Default()
+
+	userRepository := repository.NewUserRepository(database)
+	userHandler := handlers.NewUserHandler(userRepository)
+
+	router.GET("/users", userHandler.GetAllUsers)
+
+	if err := router.Run(":8080"); err != nil {
+		panic("Failed to start the server")
+	}
 }
