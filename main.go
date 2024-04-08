6ddc4b81326802b377bd9ffbbfe0cf6a3e32277d
Daniel Olearčin <dolearci@redhat.com>
2024-04-08 13:01:03 +0200

    ♻️  Extract router config to another file

--- main.go
+++ main.go
@@ -2,25 +2,10 @@ package main
 
 import (
 	"github.com/dolearci/go-microservice/config"
-	handlers "github.com/dolearci/go-microservice/handler"
-	"github.com/dolearci/go-microservice/repository"
-	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	database := config.ConnectToDatabase()
-
-	router := gin.Default()
-
-	userRepository := repository.NewUserRepository(database)
-	userHandler := handlers.NewUserHandler(userRepository)
-
-	router.GET("/", userHandler.GetAllUsers)
-	router.GET("/:id", userHandler.GetUserByID)
-	router.DELETE("/:id", userHandler.DeleteUserByID)
-	router.DELETE("/", userHandler.DeleteAllUsers)
-	router.POST("/save", userHandler.CreateUser)
-	router.PUT("/:id", userHandler.UpdateUser)
+	router := config.SetupRouter()
 
 	if err := router.Run(":8080"); err != nil {
 		panic("Failed to start the server")
